09f9518a9d86d23ec491f39556b13f66ad88305a
Torbjorn Jonsson <torbjornjonsson@macbook-air-m1.lan>
2021-03-22 15:34:37 +0100

    removed  a line

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
-    
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
